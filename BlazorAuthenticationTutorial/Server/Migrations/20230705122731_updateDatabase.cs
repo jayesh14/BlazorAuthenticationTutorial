3e6082972e65b7b3a8e53e2281a65a144d3ea75b
Jayesh <jayeshdhamsaniya@gmail.com>
2023-07-06 17:30:14 +0530

    Update Database Script

---
+++ BlazorAuthenticationTutorial/Server/Migrations/20230705122731_updateDatabase.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAuthenticationTutorial.Server.Migrations
+{
+    public partial class updateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SystemUsers_UserRole_RoleId",
+                table: "SystemUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SystemUsers_RoleId",
+                table: "SystemUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "SystemUsers");
+
+            migrationBuilder.RenameTable(
+                name: "UserRole",
+                newName: "UserRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UserRole");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "SystemUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SystemUsers_RoleId",
+                table: "SystemUsers",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SystemUsers_UserRole_RoleId",
+                table: "SystemUsers",
+                column: "RoleId",
+                principalTable: "UserRole",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
